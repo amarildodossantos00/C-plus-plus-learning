bc3442344be7b0749dc05f0c9a2c684da1ab6350
Amarildo Dos Santos <amarildodossantosoficial@gmail.com>
2025-02-21 10:39:28 +0100

    l01

---
+++ l01/sms.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void) 
+{
+  std::cout << "Dear Self.\n";
+  std::cout << "Build a first exercise on the c++ modulo-00.\n";
+  std::cout << "21/02/2025, Luanda.\n";
+}
